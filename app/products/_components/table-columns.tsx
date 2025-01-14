dffeb379fd92596b66268c61d0bf9a48dd9b2232
charles Bezerra <ch.bmoreira@gmail.com>
2025-01-14 14:26:35 -0300

    feat:format product unit price currency correction

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -39,7 +39,7 @@ export const productTableColumns: ColumnDef<Product>[] = [
     header: "Valor unitário",
     cell: (row) => {
       const product = row.row.original;
-      return Intl.NumberFormat("pt BR", {
+      return Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
       }).format(Number(product.price));
