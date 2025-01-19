2ee4730006907c18bf4634d55f0199c77b4cb262
charles Bezerra <ch.bmoreira@gmail.com>
2025-01-19 02:28:31 -0300

    feat:product stock is validated before adding product to sale

--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -71,6 +71,14 @@ const UpsertSheetContent = ({
     setSelectedProducts((currentProducts) => {
       const existingProduct = currentProducts.find(product => product.id === selectedProduct.id);
       if(existingProduct) {
+        const productIsOutOfStock = existingProduct.quantity + data.quantity > selectedProduct.stock;
+        if(productIsOutOfStock) {
+          form.setError("quantity", {
+            message: "Quantidade indisponível em estoque.",
+          });
+          return currentProducts;
+        }
+        form.reset();
         return currentProducts.map((product) => {
           if(product.id === selectedProduct.id) {
             return {
@@ -81,6 +89,14 @@ const UpsertSheetContent = ({
           return product;
         });
       }
+      const productIsOutOfStock = data.quantity > selectedProduct.stock;
+      if(productIsOutOfStock) {
+        form.setError("quantity", {
+          message: "Quantidade indisponível em estoque.",
+        });
+        return currentProducts;
+      }
+      form.reset();
         return [
           ...currentProducts,
           { ...selectedProduct,
@@ -89,7 +105,6 @@ const UpsertSheetContent = ({
           },
         ];
     });
-    form.reset();
   };
 
   const productsTotal = useMemo(() => {
